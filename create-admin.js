cecffba1613aadc98c944ea7b107f99b1d9fd221
verand97 <veramaofficial@gmail.com>
2026-07-16 11:33:22 +0700

    refactor: update eslint disable rule from no-var-requires to no-require-imports

--- create-admin.js
+++ create-admin.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
